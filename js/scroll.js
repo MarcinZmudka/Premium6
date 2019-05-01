9f1064309c7f8d98cf93c8b867d27b7ba266ee1e
Marcin Żmudka <marcinzmudka@gmail.com>
2019-05-01 23:30:05 +0200

    added bootstrap, need to thinkover slider

---
+++ js/scroll.js
@@ -0,0 +1,17 @@
+$(() =>{
+  var phone = $(".phoneS").offset().top;
+  $(window).scroll(()=>{
+    check( phone);
+  })
+});
+
+var check = function(phone){
+  var top = $(window).scrollTop();
+  var margin = $(window).height();
+  if(phone-(margin*0.05) < top){
+    $(".phoneS").addClass("sticky");
+  }
+  else{
+    $(".phoneS").removeClass("sticky");
+  }
+}
---
+++ js/slider.js
@@ -0,0 +1,64 @@
+$(()=>{
+  const slide = document.querySelector('.sliderContainer');
+  const images = document.querySelectorAll('.sliderContainer img');
+
+  const prevBtn = document.querySelector('#prevBtn');
+  const nextBtn = document.querySelector('#nextBtn');
+
+
+  var counter = 1;
+  const size = $(".slider").width();
+  slide.style.transform = 'translateX('+(-size*counter)+'px)';
+  checkPage(counter);
+
+  nextBtn.addEventListener('click', ()=>{
+    if (counter >= images.length-1) {
+      return;
+    }
+    const size = $(".slider").width();
+    slide.style.transition = "transform 0.4s ease-in-out";
+    counter++;
+    slide.style.transform = 'translateX('+(-size*counter)+'px)';
+    checkPage(counter);
+  });
+  prevBtn.addEventListener('click', ()=>{
+    const size = $(".slider").width();
+    if (counter <=0) {
+      return;
+    }
+    slide.style.transition = "transform 0.4s ease-in-out";
+    counter--;
+    slide.style.transform = 'translateX('+(-size*counter)+'px)';
+    checkPage(counter);
+  });
+  slide.addEventListener('transitionend', ()=>{
+    const size = $(".slider").width();
+    if(images[counter].id === "lastClone"){
+      slide.style.transition = "none";
+      counter = images.length-2;
+      slide.style.transform = 'translateX('+(-size*counter)+'px)';
+    }
+    if(images[counter].id === "firstClone"){
+      slide.style.transition = "none";
+      counter = images.length-counter;
+      slide.style.transform = 'translateX('+(-size*counter)+'px)';
+    }
+  });
+});
+
+function checkPage(counter){
+  var index = counter;
+  if(index == 5){
+    index =1;
+  }
+  else if (index == 0) {
+    index =4;
+  }
+  for(var i=1; i<5; i++){
+    $("#"+i).removeClass("optionS");
+    $("#"+i).addClass("option");
+  }
+  $("#"+index).addClass("optionS");
+  $("#"+index).removeClass("option");
+  console.log(counter);
+}
